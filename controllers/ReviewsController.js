8ed6cce876e5b5bc724d86dd716779f66a84a215
chris-swan <chris@chrisswan.com>
2015-10-02 16:22:11 -0700

    update text area on movies.html

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -1,7 +1,7 @@
 nicksFlicks.controller('ReviewsCtrl', function ReviewsCtrl($scope, $stateParams, MoviesFactory, UtilitiesFactory) {
-  $scope.movie = UtilitiesFactory.findById(MoviesFactory.movies, $stateParams.movieId);
-  $scope.addReview = function() {
-    $scope.movie.reviews.push({ text: $scope.review });
-    $scope.review = null;
-  }
+  $scope.movie = UtilitiesFactory.findByTitle(MoviesFactory.movies, $stateParams.movieTitle);
+  // $scope.addReview = function() {
+  //   $scope.movie.reviews.push({ text: $scope.review });
+  //   $scope.review = null;
+  // }
 });
