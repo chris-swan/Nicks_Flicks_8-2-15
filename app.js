983216bc9ed7902644c6fe34e7b7b9b3f287b6bc
chris-swan <chris@chrisswan.com>
2015-10-02 09:22:21 -0700

    add app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-app.js
+var nicksFlicks = angular.module('nicksFlicks', ['ui.router']);
+
+nicksFlicks.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: 'HeadersCtrl'
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
+  });
+
+  $stateProvider.state('movies', {
+    url: "/movies",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+      },
+      'body': {
+        templateUrl: 'partials/movies.html',
+        controller: 'moviesCtrl'
+      },
+    }
+  });
+
+  $stateProvider.state('movies.reviews', {
+    url: "/:courseId",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+      },
+      'body': {
+        templateUrl: "partials/movies.reviews.html",
+        controller: "reviewsCtrl"
+      },
+    }
+  });
+
+});
